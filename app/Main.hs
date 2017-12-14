ad614d25b7a8686380ab498001fb82198339d157
Harry Maclean <harrymaclean@gocardless.com>
2017-12-14 21:54:34 +0000

    ugh:

--- app/Main.hs
+++ app/Main.hs
@@ -6,104 +6,94 @@ module Main where
 
 import           Control.Monad.IO.Class
 import           Control.Monad.IO.Class
-import           Control.Monad.Logger               (MonadLogger)
+import           Control.Monad.Logger                 (MonadLogger)
 import           Control.Monad.Logger
-import           Control.Monad.Reader               (ask)
+import           Control.Monad.Reader                 (ask)
 import           Control.Monad.Reader
-import           Data.Aeson                         hiding (json)
-import qualified Data.Text                          as T
-import qualified Data.Text.Lazy                     as L
-import           Data.Time.Calendar                 (Day (ModifiedJulianDay),
-                                                     toModifiedJulianDay)
+import           Data.Int                             (Int64)
+import qualified Data.Text                            as T
+import qualified Data.Text.Lazy                       as L
+import           Data.Time.Calendar                   (Day (ModifiedJulianDay),
+                                                       toModifiedJulianDay)
 import           Data.Time.Clock
-import           Database.Esqueleto
+import           Database.Esqueleto                   as E
+import           Database.Esqueleto.Internal.Language (Value)
+import           Database.Esqueleto.Internal.Sql      (SqlExpr, unsafeSqlBinOp)
 import           Database.Persist
-import           Database.Persist.Postgresql
+import qualified Database.Persist.Postgresql          as P
 import           Database.PostgreSQL.Simple
 import           Database.PostgreSQL.Simple.FromRow
-import           Lib                                (QueJob)
+import           Lib                                  (EntityField (..), Lock,
+                                                       Oid, QueJob)
 import           System.Log.FastLogger
-import           Web.Scotty
-import           Web.Scotty.Internal.Types          (ActionEnv, ActionT,
-                                                     getParams)
+import           Web.Scotty                           as S
+import           Web.Scotty.Internal.Types            (ActionEnv, ActionT,
+                                                       getParams)
 
 main :: IO ()
 main = do
   conn <- connectPostgreSQL ""
   (logger, _) <- newFastLogger $ LogStdout defaultBufSize
-  backend :: SqlBackend <- openSimpleConn (nullLogger logger) conn
-  jobs <- runReaderT getJobs backend
-  mapM_ (print . toEncoding . entityVal) jobs
+  backend :: SqlBackend <- P.openSimpleConn (nullLogger logger) conn
+  runServer backend
     where nullLogger logger _ _ _ = logger
+
+runServer :: SqlBackend -> IO ()
+runServer backend = S.scotty 3001 $ do
+  S.get "/jobs" $ do
+    jobs <- liftIO $ runReaderT getJobs backend
+    json jobs
+  S.get "/locks/advisory" $ do
+    locks <- liftIO $ runReaderT getAdvisoryLocks backend
+    json locks
+  S.get "/locks" $ do
+    locks <- liftIO $ runReaderT getLocks backend
+    json locks
 
 getJobs :: MonadIO m => SqlReadT m [Entity QueJob]
 getJobs = select $ from $ (\p -> return p)
 
-data Job = Job
-  { jobPriority   :: Int
-  , jobRunAt      :: UTCTime
-  , jobId         :: Int
-  , jobClass      :: T.Text
-  , jobArgs       :: T.Text
-  , jobErrorCount :: Int
-  , jobLastError  :: T.Text
-  , jobQueue      :: T.Text
-  , jobRetryable  :: Bool
-  , jobFailedAt   :: UTCTime
-  }
+getLocks :: MonadIO m => SqlReadT m [Entity Lock]
+getLocks = select $ from $ (\p -> return p)
 
-instance ToJSON Job where
-  toJSON j = object ["id" .= jobId j]
+getAdvisoryLocks :: MonadIO m => SqlReadT m [Entity Lock]
+getAdvisoryLocks = select $ from $ \l -> do
+  where_ (l ^. LockLocktype E.==. val "advisory")
+  where_ (l ^. LockObjsubid E.==. just (val 1))
+  where_ ((((l ^. LockObjsubid) <<. Oid 32) |. (l ^. LockObjid)) E.==. (k ^. QueJobId))
+  return l
 
-instance FromRow Job where
-  fromRow =
-    Job <$> field <*> field <*> field <*> field <*> field <*> field <*> field <*>
-    field <*>
-    field <*>
-    field
+-- getLockedJobs = select $ from $ \(j, l) -> do
+-- 	where_ (j ^. QueJobId E.==. undefined)
+-- 	return (j, l)
 
+(<<.) :: (Num a, Num b, Num c) => SqlExpr (Value a) -> SqlExpr (Value b) -> SqlExpr (Value c)
+(<<.) = unsafeSqlBinOp " << "
+infix 4 <<.
 
-summary :: ActionM ()
-summary = liftIO getQueueSummary >>= json
+(|.) :: Num a => SqlExpr (Value a) -> SqlExpr (Value a) -> SqlExpr (Value a)
+(|.) = unsafeSqlBinOp " | "
+infixl 6 |.
 
-getQueueSummary :: IO [Summary]
-getQueueSummary = do
-  conn <- connectPostgreSQL ""
-  query_
-    conn
-    "SELECT job_class, priority, count(*) as count, \
-                     \sum((s.job_id is not null)::int) as count_working \
-              \FROM que_jobs \
-              \LEFT JOIN ( \
-              \   SELECT job_id \
-              \   FROM que_jobs \
-              \   JOIN ( \
-              \     SELECT ((pg_locks.classid::bigint << 32) | pg_locks.objid::bigint) as pg_lock_id \
-              \     FROM pg_locks \
-              \     WHERE pg_locks.locktype = 'advisory' \
-              \     AND pg_locks.objsubid = 1 \
-              \   ) l on l.pg_lock_id = que_jobs.job_id \
-              \) s using (job_id) \
-              \WHERE queue = '' \
-              \AND (run_at <= now() AND retryable = true) \
-              \GROUP BY job_class, priority \
-              \ORDER BY priority, lower(job_class)"
+pgLockId :: expr (Entity Lock) -> SqlExpr (Value Int64)
+pgLockId lock = (classid <<. val 32) |. objid
+  where classid = lock ^. LockClassid
+        objid = lock ^. LockObjid
 
-data Summary = Summary
-  { summaryJobClass     :: T.Text
-  , summaryPriority     :: Int
-  , summaryCount        :: Int
-  , summaryCountWorking :: Int
-  }
-
-instance ToJSON Summary where
-  toJSON s =
-    object
-      [ "job_class" .= summaryJobClass s
-      , "priority" .= summaryPriority s
-      , "count" .= summaryCount s
-      , "count_working" .= summaryCountWorking s
-      ]
-
-instance FromRow Summary where
-  fromRow = Summary <$> field <*> field <*> field <*> field
+-- "SELECT job_class, priority, count(*) as count, \
+-- 			 \sum((s.job_id is not null)::int) as count_working \
+-- \FROM que_jobs \
+-- \LEFT JOIN ( \
+-- \   SELECT job_id \
+-- \   FROM que_jobs \
+-- \   JOIN ( \
+-- \     SELECT ((pg_locks.classid::bigint << 32) | pg_locks.objid::bigint) as pg_lock_id \
+-- \     FROM pg_locks \
+-- \     WHERE pg_locks.locktype = 'advisory' \
+-- \     AND pg_locks.objsubid = 1 \
+-- \   ) l on l.pg_lock_id = que_jobs.job_id \
+-- \) s using (job_id) \
+-- \WHERE queue = '' \
+-- \AND (run_at <= now() AND retryable = true) \
+-- \GROUP BY job_class, priority \
+-- \ORDER BY priority, lower(job_class)"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveGeneric              #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
@@ -8,11 +9,17 @@
 {-# LANGUAGE TypeFamilies               #-}
 module Lib where
 
+import           Data.Aeson
+import           Data.ByteString.Read        (integral)
 import           Data.Int                    (Int64)
+import           Data.Int                    (Int8)
+import           Data.String                 (fromString)
 import qualified Data.Text                   as T
 import           Data.Time
+import           Data.Word                   (Word8)
 import           Database.Persist.Postgresql
 import           Database.Persist.TH
+import           GHC.Generics
 
 mkPersist sqlSettings [persistLowerCase|
 QueJob sql=que_jobs json
@@ -28,3 +35,49 @@ QueJob sql=que_jobs json
   failedAt UTCTime Maybe
   deriving Show
 |]
+
+mkPersist sqlSettings [persistLowerCase|
+Lock sql=pg_locks json
+  locktype String     sqltype=text
+  database Oid Maybe      sqltype=oid
+  relation Oid Maybe      sqltype=oid
+  page Int8 Maybe           sqltype=integer
+  tuple Int Maybe           sqltype=smallint
+  virtualxid String Maybe   sqltype=text
+  transactionid Xid Maybe sqltype=xid
+  classid Oid Maybe       sqltype=oid
+  objid Oid Maybe sqltype=oid
+  objsubid Int Maybe sqltype=smallint
+  Id String sql=virtualtransaction sqltype=text
+  pid Int8 Maybe sqltype=integer
+  mode String sqltype=text
+  granted Bool
+  fastpath Bool
+  deriving Show
+|]
+
+newtype Oid = Oid Word8 deriving (Show, Generic)
+instance PersistField Oid where
+  toPersistValue oid = PersistDbSpecific $ (fromString . show) oid
+  fromPersistValue (PersistDbSpecific s) = case integral s of
+                                             Just (n, _) -> Right $ Oid n
+                                             _      -> Left "Invalid OID"
+  fromPersistValue _ = Left "Invalid OID (not PersistDbSpecific)"
+instance PersistFieldSql Oid where
+  sqlType _ = SqlOther "oid"
+instance FromJSON Oid
+instance ToJSON Oid where
+  toEncoding = genericToEncoding defaultOptions
+
+newtype Xid = Xid Word8 deriving (Show, Generic)
+instance PersistField Xid where
+  toPersistValue xid = PersistDbSpecific $ (fromString . show) xid
+  fromPersistValue (PersistDbSpecific s) = case integral s of
+                                             Just (n, _) -> Right $ Xid n
+                                             _      -> Left "Invalid xid"
+  fromPersistValue _ = Left "Invalid xid (not PersistDbSpecific)"
+instance PersistFieldSql Xid where
+  sqlType _ = SqlOther "xid"
+instance FromJSON Xid
+instance ToJSON Xid where
+  toEncoding = genericToEncoding defaultOptions
